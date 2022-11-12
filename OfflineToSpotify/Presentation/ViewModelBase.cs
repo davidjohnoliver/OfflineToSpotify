296c0c284a0721005e44d43fa04cca323ddc39b3
David Oliver <david.john.oliver@gmail.com>
2022-11-12 05:54:18 -0500

    Add ViewModelBase and SimpleCommand

---
+++ OfflineToSpotify/Presentation/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Presentation
+{
+	internal class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		/// <summary>
+		/// Call from a bindable property to raise the <see cref="PropertyChanged"/> event when needed.
+		/// </summary>
+		/// <param name="currentValue">Pass the backing field here.</param>
+		/// <param name="newValue">Pass the set value here.</param>
+		/// <returns>True if the value changed, false if the new value is equal to the previous.</returns>
+		protected bool OnValueSet<T>(ref T currentValue, T newValue, [CallerMemberName] string? name = null)
+		{
+			if (!Equals(currentValue, newValue))
+			{
+				currentValue = newValue;
+				OnPropertyChanged(name);
+				return true;
+			}
+
+			return false;
+		}
+
+		private void OnPropertyChanged(string? name)
+			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
