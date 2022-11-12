cf19cfad5243db5b3c68aba181f24c776de9534f
David Oliver <david.john.oliver@gmail.com>
2022-11-12 05:54:02 -0500

    Add m3u import

---
+++ OfflineToSpotify.Import/ImportUtil.cs
@@ -0,0 +1,26 @@
+﻿using OfflineToSpotify.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Import;
+
+public static class ImportUtil
+{
+	public static IEnumerable<TrackInfo> ImportPlaylist(string playlistPath)
+	{
+		foreach (var line in File.ReadLines(playlistPath))
+		{
+			yield return ImportTrack(line);
+		}
+	}
+
+	private static TrackInfo ImportTrack(string trackPath)
+	{
+		using var tf = TagLib.File.Create(trackPath);
+
+		return new TrackInfo(tf.Tag.FirstPerformer, tf.Tag.Title, tf.Tag.Album);
+	}
+}
