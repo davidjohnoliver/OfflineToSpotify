0caa20a84a25bbeff2bf42bb477ca5d48320c38a
David Oliver <david.john.oliver@gmail.com>
2022-11-13 21:21:00 -0500

    Add Navigator page wrapper

---
+++ OfflineToSpotify/Presentation/Navigator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Presentation
+{
+	internal class Navigator<TParam>
+	{
+		private readonly Page _page;
+
+		public Navigator(Page page)
+		{
+			_page = page;
+		}
+
+		public bool Navigate<TPage>(TParam parameter) where TPage : Page
+			=> _page.Frame.Navigate(typeof(TPage), parameter);
+	}
+}
