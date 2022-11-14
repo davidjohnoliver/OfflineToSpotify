0822acfdb610274eddeed5f3a8a1d8907ff9aa6a
David Oliver <david.john.oliver@gmail.com>
2022-11-13 21:21:14 -0500

    Add SettingsUtil

---
+++ OfflineToSpotify/Utils/SettingsUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace OfflineToSpotify.Utils
+{
+	public static class SettingsUtil
+	{
+		public static T GetSavedValue<T>(string key)
+		{
+			if (ApplicationData.Current.LocalSettings.Values.TryGetValue(key, out var value))
+			{
+				return (T)value;
+			}
+
+			return default;
+		}
+
+		public static void SaveValue<T>(string key, T value)
+		{
+			ApplicationData.Current.LocalSettings.Values[key] = value;
+		}
+	}
+}
