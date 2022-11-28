2f75ed2ff4e3bbf1b659a19e24833d6f06ab2f78
David Oliver <david.john.oliver@gmail.com>
2022-11-27 19:20:17 -0500

    BooleanToVisibilityConverter

---
+++ OfflineToSpotify/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Converters
+{
+	public sealed class BooleanToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language) => value switch
+		{
+			true => Visibility.Visible,
+			false => Visibility.Collapsed,
+			_ => null
+		};
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
