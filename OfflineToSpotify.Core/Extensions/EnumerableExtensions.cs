4eb332140adbcdde8e7ba820da48af1f30575440
David Oliver <david.john.oliver@gmail.com>
2022-12-19 16:39:36 -0500

    Add Indexed extension

---
+++ OfflineToSpotify.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Core.Extensions
+{
+	public static class EnumerableExtensions
+	{
+		public static IEnumerable<(T Value, int Index)> Indexed<T>(this IEnumerable<T> collection)
+			=> collection.Select((t, i) => (t, i));
+	}
+}
