0e2b1ea0319c521c2d0f6826e195c2ce39ba1bad
David Oliver <david.john.oliver@gmail.com>
2022-12-24 05:25:55 -0500

    Add ListUtils.FromValues

---
+++ OfflineToSpotify.Core/Utils/ListUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Core.Utils
+{
+	public static class ListUtils
+	{
+		public static List<T> FromValues<T>(params T[] values) => values.ToList();
+	}
+}
