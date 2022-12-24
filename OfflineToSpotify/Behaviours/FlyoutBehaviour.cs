8695d042b4e7b979e52ee491cb06161f210363ff
David Oliver <david.john.oliver@gmail.com>
2022-12-24 07:09:18 -0500

    Allow to propagate flyout as dismissible

--- OfflineToSpotify/Behaviours/FlyoutBehaviour.cs
+++ OfflineToSpotify/Behaviours/FlyoutBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls.Primitives;
+using OfflineToSpotify.Presentation;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -45,6 +46,39 @@ namespace OfflineToSpotify.Behaviours
 					args.Cancel = true;
 				}
 				;
+			}
+		}
+
+
+		public static FlyoutBase GetSetAsDismissible(DependencyObject obj)
+		{
+			return (FlyoutBase)obj.GetValue(SetAsDismissibleProperty);
+		}
+
+		public static void SetSetAsDismissible(DependencyObject obj, FlyoutBase value)
+		{
+			obj.SetValue(SetAsDismissibleProperty, value);
+		}
+
+		// Using a DependencyProperty as the backing store for SetAsDismissible.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty SetAsDismissibleProperty =
+			DependencyProperty.RegisterAttached("SetAsDismissible", typeof(FlyoutBase), typeof(FlyoutBehaviour), new PropertyMetadata(null, OnSetAsDismissibleChanged));
+
+		private static void OnSetAsDismissibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var flyoutChild = (FrameworkElement)d;
+
+			if (e.NewValue is FlyoutBase flyout)
+			{
+				flyoutChild.DataContextChanged += FlyoutChild_DataContextChanged;
+				;
+				void FlyoutChild_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+				{
+					if (args.NewValue is IReceiver<IDismissibleContainer> receiver)
+					{
+						receiver.Set(new FlyoutDismissibleContainer(flyout));
+					}
+				}
 			}
 		}
 
---
+++ OfflineToSpotify/Presentation/FlyoutDismissibleContainer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Presentation
+{
+	public class FlyoutDismissibleContainer : IDismissibleContainer
+	{
+		private readonly FlyoutBase _flyout;
+
+		public FlyoutDismissibleContainer(FlyoutBase flyout)
+		{
+			_flyout = flyout;
+		}
+
+		public void Dismiss()
+		{
+			_flyout.Hide();
+		}
+	}
+}
