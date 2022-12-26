d53b2d3bb233ce17f024457f151a1328bc6aed52
David Oliver <david.john.oliver@gmail.com>
2022-12-26 11:19:44 -0500

    Add typed TemplateSelector

---
+++ OfflineToSpotify/Conversion/TemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Conversion
+{
+	public abstract class TemplateSelector<T> : DataTemplateSelector
+	{
+		protected override sealed DataTemplate SelectTemplateCore(object item) => SelectTemplatePrivate(item);
+
+		protected override sealed DataTemplate SelectTemplateCore(object item, DependencyObject container)
+			=> SelectTemplatePrivate(item);
+
+		private DataTemplate SelectTemplatePrivate(object obj) => obj switch
+		{
+			T item => SelectTemplateInner(item),
+			_ => SelectDefaultTemplate()
+		};
+
+		protected abstract DataTemplate SelectTemplateInner(T item);
+
+		protected virtual DataTemplate SelectDefaultTemplate() => null;
+	}
+}
