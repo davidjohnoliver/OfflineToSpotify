74a684956166d73ab357fb846196918004c4661c
David Oliver <david.john.oliver@gmail.com>
2022-12-26 11:21:01 -0500

    Add ListExtensions.ContainsIndex()

---
+++ OfflineToSpotify.Core/Extensions/ListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineToSpotify.Core.Extensions
+{
+	public static class ListExtensions
+	{
+		public static bool ContainsIndex<T>(this IList<T> list, int index) => index >= 0 && index < list.Count;
+	}
+}
