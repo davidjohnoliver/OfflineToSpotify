796738841787e216035f855d0b319991c8f2c925
David Oliver <david.john.oliver@gmail.com>
2022-12-25 19:19:33 -0500

    Rename Converters -> Conversion

--- OfflineToSpotify/Conversion/BooleanToVisibilityConverter.cs
+++ OfflineToSpotify/Conversion/BooleanToVisibilityConverter.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OfflineToSpotify.Converters
+namespace OfflineToSpotify.Conversion
 {
 	public sealed class BooleanToVisibilityConverter : IValueConverter
 	{
